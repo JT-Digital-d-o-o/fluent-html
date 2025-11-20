f4157106052380b4aa51582a2a48d1b707f696d0
Toni K. Turk <kocjanturk@gmail.com>
2025-11-20 07:57:24 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export { render, Thunk } from './builder.js';
 export { View, Tag, ButtonTag, InputTag, TextareaTag, ImgTag, AnchorTag, LabelTag, SelectTag, FormTag } from './builder.js';
-export { Div, HStack, Main, Header, Footer, Section, El, Form, H1, H2, H3, H4, Input, Label, Button, P, A, Empty, Img, Ul, Li, Table, Thead, Td, Th, Tr, Tbody, Select, Option, Textarea, Hr, Overlay, OverlayPosition, IfThen, IfThenElse, SwitchCase, ForEach, ForEach1, ForEach2, ForEach3 } from './builder.js';
-export { Text } from './builder.js';
-export { VStack } from './builder.js';
+export { Div, HStack, Main, Header, Footer, Section, El, Form, H1, H2, H3, H4, Input, Label, Button, P, A, Empty, Img, Ul, Li, Span, Table, Thead, Td, Th, Tr, Tbody, Select, Option, Textarea, Hr, Overlay, OverlayPosition, IfThen, IfThenElse, SwitchCase, ForEach, ForEach1, ForEach2, ForEach3 } from './builder.js';
 export { hx, HTMX, HxSwap, HxTrigger, HxEncoding, HxTarget, HxHttpMethod, id, clss } from './htmx.js';
-export { ElOld, DivOld, FormOld, H1Old, H2Old, H3Old, H4Old, LabelOld, InputOld, POld, AOld, ImgOld, UlOld, LiOld, SelectOld, TextareaOld, TableOld, TheadOld, TdOld, ThOld, TrOld, TbodyOld, HrOld, ButtonOld, SpanOld, OverlayOld } from './builder.js';
